837cb6dd899e77505ae2f7fe7211999fbeb07306
azure.lu <azure.lu@ap.jll.com>
2018-06-22 11:23:04 +0800

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request.');
-    res.end('Hello World!');
+    res.end('Hello World from Azure Git!');
 }
 
 const server = http.createServer(handler);
