f2d37793473924184ae46cb279de3b7a19e4b76d
azure.lu <azure.lu@ap.jll.com>
2018-06-22 11:22:28 +0800

    update commit

--- azure-shell-tdim-simple-code.sh
+++ azure-shell-tdim-simple-code.sh
@@ -8,18 +8,18 @@ RUNTIME="node|8.1"
 
 # login supports device login, username/password, and service principals
 # see https://docs.microsoft.com/en-us/cli/azure/?view=azure-cli-latest#az_login
-# az login
-# # list all of the available subscriptions
-# az account list -o table
-# # set the default subscription for subsequent operations
-# az account set --subscription $SUBSCRIPTION
-# # create a resource group for your application
-# az group create --name $RESOURCEGROUP --location $LOCATION
-# # create an appservice plan (a machine) where your site will run
-# az appservice plan create --name $PLANNAME --location $LOCATION --is-linux --sku $PLANSKU --resource-group $RESOURCEGROUP
-# # create the web application on the plan
-# # specify the node version your app requires
-# az webapp create --name $SITENAME --plan $PLANNAME --runtime $RUNTIME --resource-group $RESOURCEGROUP
+az.cmd login
+# list all of the available subscriptions
+az.cmd account list -o table
+# set the default subscription for subsequent operations
+az.cmd account set --subscription $SUBSCRIPTION
+# create a resource group for your application
+az.cmd group create --name $RESOURCEGROUP --location $LOCATION
+# create an appservice plan (a machine) where your site will run
+az.cmd appservice plan create --name $PLANNAME --location $LOCATION --is-linux --sku $PLANSKU --resource-group $RESOURCEGROUP
+# create the web application on the plan
+# specify the node version your app requires
+az.cmd webapp create --name $SITENAME --plan $PLANNAME --runtime $RUNTIME --resource-group $RESOURCEGROUP
 
 # To set up deployment from a local git repository, uncomment the following commands.
 # first, set the username and password (use environment variables!)
